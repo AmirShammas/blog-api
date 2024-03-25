7ac8b8a988737797277e3157800a03059efe97dc
ShammasAmir <amirshammas@gmail.com>
2024-03-25 13:27:24 +0330

    Create CustomUser model.

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    #MyApps
+    # MyApps
     "accounts.apps.AccountsConfig",
 ]
 
@@ -112,3 +112,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
