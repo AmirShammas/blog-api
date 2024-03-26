d7700273631c7f676c0cec8316ff0e858ad87305
ShammasAmir <amirshammas@gmail.com>
2024-03-26 10:48:04 +0330

    Add redoc and swagger.

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    "drf_spectacular",
 ]
 
 REST_FRAMEWORK = {
@@ -52,6 +53,7 @@ REST_FRAMEWORK = {
         # "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 AUTHENTICATION_BACKENDS = [
@@ -160,3 +162,10 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog to learn about DRF",
+    "VERSION": "1.0.0",
+    # OTHER SETTINGS
+}
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView,)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/", include("posts.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/dj-rest-auth/", include("dj_rest_auth.urls")),
     path("api/dj-rest-auth/registration/",
          include("dj_rest_auth.registration.urls")),
     path('api/accounts/', include('allauth.urls')),
+    path("api/schema/redoc/",
+         SpectacularRedocView.as_view(url_name="schema"), name="redoc",),
+    path("api/schema/swagger-ui/",
+         SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
